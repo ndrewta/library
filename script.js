fdb35a1cd26a3d28740dc1b82f6d336b4968a390
Andrew T <andrewtgit@gmail.com>
2023-02-25 15:33:03 +1100

    Added functions to submit form data, create book object and update page.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let library = [];
+
 const bookContainer = document.querySelector(".book-container");
+const formElem = document.querySelector("form");
+const formDiv = document.querySelector(".form");
+
+formElem.addEventListener("submit", (e) => submit(e));
+
+const addBookBtn = document.querySelector("#add-book");
+addBookBtn.addEventListener("click", toggleForm);
+const cancelBtn = document.querySelector("#cancel");
+cancelBtn.addEventListener("click", toggleForm);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,6 +21,26 @@ function Book(title, author, pages, read) {
 Book.prototype.pushBookToLibrary = function () {
   library.push(this);
 };
+
+function submit(e) {
+  e.preventDefault();
+
+  const data = new FormData(formElem);
+  const title = data.get("title");
+  const author = data.get("author");
+  const pages = data.get("pages");
+  const read = data.get("read");
+
+  createBook(title, author, pages, read);
+  formElem.reset();
+}
+
+function createBook(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  book.pushBookToLibrary();
+  updateLibrary(book);
+  toggleForm();
+}
 
 function loopLibrary() {
   library.forEach((book) => {
@@ -24,9 +54,18 @@ function loopLibrary() {
   });
 }
 
+function updateLibrary(book) {
+  const cover = document.createElement("div");
+  cover.setAttribute("class", "book-cover");
+  cover.textContent = `Title: ${book.title}\n`;
+  cover.textContent += `Author: ${book.author}\n`;
+  cover.textContent += `Pages: ${book.pages}\n`;
+  cover.textContent += `Read: ${book.read}`;
+  bookContainer.appendChild(cover);
+}
+
 function toggleForm() {
-  const element = document.querySelector(".form");
-  element.classList.toggle("form-toggle");
+  formDiv.classList.toggle("form-toggle");
 }
 
 const bookA = new Book("Harry Pooter", "J Rowling", 290);
@@ -36,5 +75,3 @@ bookB.pushBookToLibrary();
 const bookC = new Book("House of Horrors", "Cowabunga", 392, "yes");
 bookC.pushBookToLibrary();
 loopLibrary();
-const toggleBtn = document.querySelector("#toggle-btn");
-toggleBtn.addEventListener("click", toggleForm);
