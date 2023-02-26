b16dcabc121f4a0adb032b29189a7dc47919cc7a
Andrew T <andrewtgit@gmail.com>
2023-02-26 13:41:14 +1100

    Added remove book function.

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ let library = [];
 const bookContainer = document.querySelector(".book-container");
 const formElem = document.querySelector("form");
 const formDiv = document.querySelector(".form");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
+const notReadInput = document.getElementById("not-read");
+const submitBtn = document.getElementById("submit");
+const addBookBtn = document.getElementById("add-book");
+const cancelBtn = document.getElementById("cancel");
 
-formElem.addEventListener("submit", (e) => submit(e));
-
-const addBookBtn = document.querySelector("#add-book");
-addBookBtn.addEventListener("click", toggleForm);
-const cancelBtn = document.querySelector("#cancel");
-cancelBtn.addEventListener("click", toggleForm);
+const formInputElem = [
+  titleInput,
+  authorInput,
+  pagesInput,
+  readInput,
+  notReadInput,
+  submitBtn,
+  cancelBtn,
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -21,6 +32,55 @@ function Book(title, author, pages, read) {
 Book.prototype.pushBookToLibrary = function () {
   library.push(this);
 };
+
+function loopLibrary() {
+  library.forEach((book) => {
+    const cover = document.createElement("div");
+    cover.setAttribute("class", "book-cover");
+    cover.textContent = `Title: ${book.title}\n`;
+    cover.textContent += `Author: ${book.author}\n`;
+    cover.textContent += `Pages: ${book.pages}\n`;
+    cover.textContent += `Read: ${book.read}`;
+    const btn = document.createElement("button");
+    btn.textContent = "Remove";
+    btn.setAttribute("id", "cover-button");
+    cover.appendChild(btn);
+    bookContainer.appendChild(cover);
+  });
+}
+
+function updateLibrary(book) {
+  const cover = document.createElement("div");
+  cover.setAttribute("class", "book-cover");
+  cover.textContent = `Title: ${book.title}\n`;
+  cover.textContent += `Author: ${book.author}\n`;
+  cover.textContent += `Pages: ${book.pages}\n`;
+  cover.textContent += `Read: ${book.read}`;
+  const btn = document.createElement("button");
+  btn.textContent = "Remove";
+  btn.setAttribute("id", "cover-button");
+  cover.appendChild(btn);
+  bookContainer.appendChild(cover);
+}
+
+function toggleForm() {
+  formDiv.classList.toggle("form-toggle");
+
+  formInputElem.forEach((elem) => {
+    if (elem.disabled === true) {
+      elem.disabled = false;
+    } else {
+      elem.disabled = true;
+    }
+  });
+}
+
+function createBook(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  book.pushBookToLibrary();
+  updateLibrary(book);
+  toggleForm();
+}
 
 function submit(e) {
   e.preventDefault();
@@ -35,37 +95,10 @@ function submit(e) {
   formElem.reset();
 }
 
-function createBook(title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
-  book.pushBookToLibrary();
-  updateLibrary(book);
-  toggleForm();
-}
-
-function loopLibrary() {
-  library.forEach((book) => {
-    const cover = document.createElement("div");
-    cover.setAttribute("class", "book-cover");
-    cover.textContent = `Title: ${book.title}\n`;
-    cover.textContent += `Author: ${book.author}\n`;
-    cover.textContent += `Pages: ${book.pages}\n`;
-    cover.textContent += `Read: ${book.read}`;
-    bookContainer.appendChild(cover);
-  });
-}
-
-function updateLibrary(book) {
-  const cover = document.createElement("div");
-  cover.setAttribute("class", "book-cover");
-  cover.textContent = `Title: ${book.title}\n`;
-  cover.textContent += `Author: ${book.author}\n`;
-  cover.textContent += `Pages: ${book.pages}\n`;
-  cover.textContent += `Read: ${book.read}`;
-  bookContainer.appendChild(cover);
-}
-
-function toggleForm() {
-  formDiv.classList.toggle("form-toggle");
+function removeCover(cover) {
+  if (cover.id === "cover-button") {
+    cover.parentElement.remove();
+  }
 }
 
 const bookA = new Book("Harry Pooter", "J Rowling", 290);
@@ -75,3 +108,8 @@ bookB.pushBookToLibrary();
 const bookC = new Book("House of Horrors", "Cowabunga", 392, "yes");
 bookC.pushBookToLibrary();
 loopLibrary();
+
+addBookBtn.addEventListener("click", toggleForm);
+cancelBtn.addEventListener("click", toggleForm);
+formElem.addEventListener("submit", (e) => submit(e));
+document.addEventListener("click", (e) => removeCover(e.target));
